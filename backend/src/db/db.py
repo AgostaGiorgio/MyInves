78ea0a86ef29c21e5fdd59b01b544d4b8cb7f773
AgostaGiorgio <agogi.dev@gmail.com>
2026-03-17 21:02:00 +0400

    fix: minor changes in the backend and frontend

--- backend/src/db/db.py
+++ backend/src/db/db.py
@@ -2,5 +2,5 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from src.config.app_config import app_config
 
 
-engine = create_async_engine(app_config.postgresql_url, echo=False)
+engine = create_async_engine(app_config.postgresql_connection_uri, echo=False)
 AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
