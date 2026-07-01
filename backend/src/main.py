30a1816b0622acc7abef6d1f1f01de00edd90cc5
AgostaGiorgio <agogi.dev@gmail.com>
2026-07-01 22:21:28 +0400

    feat: add orbit client

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,16 +1,34 @@
 import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from src.di import Container
 from src.routers import router
+from src.clients.orbit_client import OrbitClient
 from src.config.app_config import app_config
 
 logger = logging.getLogger(__name__)
 
+if app_config.orbit_api_url:
+    orbit_client = OrbitClient(
+        orbit_api_url=app_config.orbit_api_url,
+        name="MyInves",
+        version="1.1.0",
+        description="Investment tracker",
+        app_url="https://myinves.agogi.dev"
+    )
+    
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if app_config.orbit_api_url:
+        orbit_client.start()
+        yield
+        orbit_client.stop()
+
 container = Container()
 container.wire(modules=[router])
 
-app = FastAPI(title="MyInves API", version="0.1.0")
+app = FastAPI(lifespan=lifespan, title="MyInves API", version="1.1.0")
 
 app.add_middleware(
     CORSMiddleware,
